4ab6fb929842c539ee065ccae80a7a621e157bca
goliatone <burgosemi@gmail.com>
2018-02-08 21:06:21 -0500

    add bin srcs

---
+++ src/bin/cli-commands.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const ServicePingCommand = require('../commands/service.ping');
+const InsightsGetCommand = require('../commands/insights.get');
+const ServiceIdInsightsCommand = require('../commands/service.id.insights');
+
+/**
+ * Attach available commands to the running cli instance.
+ * 
+ * @param {CliApp} cli 
+ * @param {String} namespace 
+ */
+module.exports.attach = function $attach(cli, namespace = false) {
+    const config = {
+        namespace,
+        prog: cli.prog,
+        context: cli.context
+    };
+
+    ServicePingCommand.attach(config);
+    InsightsGetCommand.attach(config);
+    ServiceIdInsightsCommand.attach(config);
+    
+};
