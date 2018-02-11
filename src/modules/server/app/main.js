768b908cf0a6df6203de54a8550c3b85b5779c86
goliatone <burgosemi@gmail.com>
2018-02-10 22:45:09 -0500

    update application add view

--- src/modules/server/app/main.js
+++ src/modules/server/app/main.js
@@ -4,6 +4,6 @@ import App from './Application.html';
 
 const app = new App({
     //this breaks the layout, it does not support being wrapped by div
-    // target: document.querySelector('#app')
+    // target: document.querySelector('#wrapper')
     target: document.getElementsByTagName('body')[0]
 });
