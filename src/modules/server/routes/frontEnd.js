e36001ae2cc3264d4183e4c22c8ea20aa4acb1ac
goliatone <burgosemi@gmail.com>
2018-02-10 22:42:06 -0500

    add application routes for front-end

--- src/modules/server/routes/frontEnd.js
+++ src/modules/server/routes/frontEnd.js
@@ -4,7 +4,14 @@ module.exports = function(router, config, subapp) {
     
     console.log('mouting route "Test"', arguments.length);
 
-    subapp.get('/', function(req, res){
+    subapp.get('/', function(req, res) {
+        res.render('index', {
+            message:'world',
+            content: 'this is sparta'
+        });
+    });
+
+    subapp.get('/application*', function(req, res) {
         res.render('index', {
             message:'world',
             content: 'this is sparta'
