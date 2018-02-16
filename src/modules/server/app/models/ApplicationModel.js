cf5e10925aafc17b852c9ecce409110553e84cf0
goliatone <burgosemi@gmail.com>
2018-02-15 22:25:08 -0500

    add application model

---
+++ src/modules/server/app/models/ApplicationModel.js
@@ -0,0 +1,44 @@
+'use strict';
+import extend from'gextend';
+import {ServiceModel} from './ServiceModel';
+
+const defaults = {
+    attributes: [
+        'uuid', 
+        'identifier', 
+        'appId', 
+        'environment', 
+        'data', 
+        'hostname',
+        'createdAt',
+        'updatedAt',
+        'online',
+    ]
+};
+
+export class ApplicationModel {
+    constructor(data) {
+        if(data.vo) {
+            this.fromVO(data.vo);
+        }
+    }
+
+    fromVO(vo) {
+        console.log('VO', vo);
+        extend(this, vo);
+    }
+
+    sayHello() {
+        return 'Hello ' + this.appId;
+    }
+
+    set service(vo) {
+        this._service = new ServiceModel({vo});
+    }
+
+    get service() {
+        return this._service;
+    }
+}
+
+
