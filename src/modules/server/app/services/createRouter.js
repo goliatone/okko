9ea61d735794207d3b0b43b2858705e871c08a24
goliatone <burgosemi@gmail.com>
2018-02-17 19:40:40 -0500

    update create router service

--- src/modules/server/app/services/createRouter.js
+++ src/modules/server/app/services/createRouter.js
@@ -1,35 +1,57 @@
-import Route from 'route-parser';
+'use strict';
 import history from './history';
-
-
+import Route from 'route-parser';
 
 /**
- * Refactor router.
- * See:
- * formalized: https://github.com/jikkai/svelte-router
- * added qs: https://github.com/TehShrike/svelte-querystring-router
+ * Create router
+ * Routes are strings that map to an svelte
+ * component.
  * 
- * https://github.com/shanewwarren/svelte-router5
- * https://github.com/router5/router5
- *
+ * 
+ * @param {Object} routes Configuration object
  */
-
-export default routes => {
-    let content;
-    let unlisten;
+const createRouter = routes => {
+    /** 
+     * DOM element to attach content/pages.
+     */
     let target;
+    /** 
+     * Component instance, we save a 
+     * reference to created components
+     * when our route object is a function.
+     */
+    let content;
+    /** 
+     * history listener  
+     */
+    let unlisten;
 
     const createRouteBehavior = route => {
+        /**
+         * Handle functins, in our case 
+         * those are usually svelte Components.
+         */
         if (typeof route === 'function') {
-            return input => (content = new route(input));
+            const Component = route;
+            return input => (content = new Component(input));
         }
 
+        /**
+         * We support internal redirects as 
+         * object:
+         * '/route/redirect': {redirect: '/home'}
+         */
         if (typeof route === 'object') {
             if (route.redirect) {
                 return () => history.push(route.redirect);
             }
         }
 
+        /**
+         * We support internal redirects as 
+         * strings:
+         * '/route/redirect': '/home'
+         */
         if (typeof route === 'string') {
             return () => history.push(route);
         }
@@ -37,21 +59,73 @@ export default routes => {
         return () => {};
     };
 
+    /** 
+     * Parse the routes object. Here we
+     * build our entries in route/behavior
+     * objects.
+     * 
+     * TODO: This should be configurable.
+     * We might want to have a diferent
+     * way to define routes, say we want 
+     * to be able to name routes so we 
+     * can then either trigger them by 
+     * name or reverse the route from 
+     * a name.
+    */
     const routeData = Object.keys(routes)
-        .map(key => [key, routes[key]])
-        .map(([key, value]) => ({
-            route: new Route(key),
+        .map(path => [path, routes[path]])
+        .map(([path, value]) => ({
+            route: new Route(path),
             behavior: createRouteBehavior(value)
         }));
 
-    const handleRouteChange = location => {
+    /**
+     * The `location` argument implements a subset
+     * of the `window.location` interface:
+     * - location.pathname
+     * - location.search
+     * - location.hash
+     * 
+     * Action might be one of:
+     * - POP
+     * - PUSH
+     * - REPLACE
+     * 
+     * @argument {Object} location
+     */     
+    const handleRouteChange = (location, action) => {
+        console.warn('handle route change', location);
+        //We might want to do this in middleware?
+        // if(location.pathname === history.location.pathname) {
+        //     if(location.hash && content) {
+        //         return content.set({hash: location.hash});
+        //     }
+        // }
+
         if (content && content.teardown) content.teardown();
 
         for (let i = 0; i < routeData.length; i += 1) {
+            /** 
+             * data will be false when the route does not
+             * match location. In case is a match will return
+             * either an empty object or an object with 
+             * captured parameters.
+             */
             const data = routeData[i].route.match(location.pathname);
+            
             if (data) {
-                routeData[i].behavior({ target, data });
-
+                /**
+                 * data and target are relevant for
+                 * Components. 
+                 * - target: Component will render to it.
+                 * - data: All captured parameters
+                 * will be available to components on
+                 * their data object.
+                 */
+                routeData[i].behavior({ 
+                    data, 
+                    target, 
+                });
                 break;
             }
         }
@@ -59,15 +133,77 @@ export default routes => {
 
     return {
         start: (location, targetElement) => {
+            
             target = targetElement;
+
+            /**
+             * Listen for changes in URL and handle
+             * accordingly.
+             */
             unlisten = history.listen(handleRouteChange);
+            
             handleRouteChange(location);
         },
         teardown: () => {
-            if (unlisten) {
-                unlisten();
-                unlisten = undefined;
-            }
-        }
+            if (!unlisten) return;
+            unlisten();
+            unlisten = undefined;
+        },
+        go: _go,
+        goto: _goto,
+        replace: _replace,
+        goForward: _goForward,
+        goBack: _goBack,
+        listen: _listen,
     };
 };
+
+function _goto(path) {
+    history.push(path);
+}
+
+function _goBack() {
+    history.goBack();
+}
+
+function _goForward() {
+    history.goForward();
+}
+
+function _replace(path){
+    history.replace(path);
+}
+
+function _go(n){
+    history.go(n);
+}
+
+function _listen(fn) {
+    return history.listen(fn);
+}
+
+/**
+ * Push a new entry onto the history stack. 
+ * @param {String} path URI
+ */
+createRouter.goto = _goto;
+
+/**
+ * Expose goBack to go back in the
+ * history stack. 
+ */
+createRouter.goBack = _goBack;
+
+/**
+ * Expose goForward to go back in the
+ * history stack. 
+ */
+createRouter.goForward = _goForward;
+
+createRouter.replace = _replace;
+
+createRouter.go = _go;
+
+createRouter.listen = _listen;
+
+export default createRouter;
