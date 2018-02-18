08c8b0054ae1dc98fcba0256e60c9543d6b1bb0a
goliatone <burgosemi@gmail.com>
2018-02-17 19:34:18 -0500

    Add model stores and models to api

--- src/modules/server/app/services/api.js
+++ src/modules/server/app/services/api.js
@@ -1,66 +1,61 @@
 'use strict';
-import {ApplicationModel} from '../models/ApplicationModel';
+import { ServiceModel } from '../models/ServiceModel';
+import { InsightModel } from '../models/InsightModel';
+import { ApplicationModel } from '../models/ApplicationModel';
 
-const cache = {};
-cache.applications = {};
-cache.services = {};
-cache.insights = {};
+import { ServiceCollection } from '../models/ServiceCollection';
+import { InsightsCollection } from '../models/InsightsCollection';
+import { ApplicationCollection } from '../models/ApplicationCollection';
 
 function createApplication(data) {
     return fetch('/api/application', {
         method: 'POST',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
+            Accept: 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    .then(res => {
-        return res.json();
-    })
-    .then(json => {
-        let vo = json.value;
-        cache.applications[json.value.id] = vo;
-        let model = new ApplicationModel({vo});
-        return json.value;
-    });
+        .then(res => {
+            return res.json();
+        })
+        .then(json => {
+            return new ApplicationModel({
+                vo: json.value
+            });
+        });
 }
 
 function updateApplication(id, data) {
     return fetch('/api/application/' + id, {
         method: 'PUT',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
+            Accept: 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    .then(res => {
-        return res.json();
-    })
-    .then(json => {
-        cache.applications[json.value.id] = json.value;
-        return json.value;
-    });
+        .then(res => {
+            return res.json();
+        })
+        .then(json => {
+            return new ApplicationModel({
+                vo: json.value
+            });
+        });
 }
 
 function deleteApplication(id) {
-    if(typeof id === 'object') id = id.id;
+    if (typeof id === 'object') id = id.id;
 
     return fetch('/api/application/' + id, {
         method: 'DELETE',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
+            Accept: 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         }
-    })
-    .then(res => {
+    }).then(res => {
         return res.json();
-    })
-    .then(json => {
-        json = cache.applications[id];
-        delete cache.applications[id];
-        return json;
     });
 }
 
@@ -70,10 +65,9 @@ function getApplication(id) {
             return res.json();
         })
         .then(json => {
-            let vo = json.value;
-            cache.applications[json.value.id] = vo;
-            let model = new ApplicationModel({vo});
-            return model;
+            return new ApplicationModel({
+                vo: json.value
+            });
         });
 }
 
@@ -82,44 +76,34 @@ function getApplications() {
         .then(res => {
             return res.json();
         })
-        .then(json => {
-            
-            json.value.map(app => {
-                console.log('id', app.id);
-                let model = new ApplicationModel({vo:app});
-                cache.applications[app.id] = app;
-                return model;
-            });
-
-            return json;
+        .then(results => {
+            return new ApplicationCollection({results});
         });
 }
 
 function getServices() {
-    return fetch('/api/services').then(res => {
-        return res.json();
-    })
-    .then(json => {
-        
-        json.value.map(service => {
-            cache.services[service.id] = service;
+    return fetch('/api/services')
+        .then(res => {
+            return res.json();
+        })
+        .then(results => {
+            return new ServiceCollection({results});
         });
-
-        return json;
-    });
 }
 
 function getInsights() {
     return fetch('/api/insights').then(res => {
         return res.json();
+    })
+    .then(results => {
+        return new InsightsCollection({results});
     });
 }
 
 function getInsightsFor(application) {
-    return getInsights().then(json => {
-        let value = json.value;
-        return value.find((insight)=>{
-            return insight.service.id = application;
+    return getInsights().then(insights => {
+        return insights.find(insight => {
+            return (insight.application = application);
         });
     });
 }
@@ -133,10 +117,6 @@ function buildApplicationInsights() {
     return new Promise((resolve, reject) => {
         Promise.all([services, applications, insights]).then(
             ([services, applications, insights]) => {
-                services = services.value;
-                applications = applications.value;
-                insights = insights.value;
-
                 services.map(service => {
                     applications.map(application => {
                         if (application.id === service.application) {
@@ -148,7 +128,6 @@ function buildApplicationInsights() {
                                 }
                             });
                         }
-                        
                     });
                 });
 
@@ -159,7 +138,6 @@ function buildApplicationInsights() {
 }
 
 export default {
-    cache,
     getInsights,
     getApplication,
     getApplications,
